ba52252d9c0c796fa3d4a12e3a0d8e1f71f11267
unsivil <unsivilaudio@gmail.com>
2020-10-19 13:10:58 -0600

    fix palette routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import Palette from './Palette';
 import seedColors from '../seedColors';
 import { generatePalette } from '../utils/chroma';
+import '../styles/App.css';
 
 const app = props => {
+    function findPalette(id) {
+        return seedColors.find(el => el.id === id);
+    }
+
     return (
-        <div>
-            <Palette palette={generatePalette(seedColors[4])} />
+        <div className='App'>
+            <Switch>
+                <Route
+                    exact
+                    path='/'
+                    render={() => <h1>This is the mini-list page</h1>}
+                />
+                <Route
+                    exact
+                    path='/palette/:id'
+                    render={({ match }) => (
+                        <Palette
+                            palette={generatePalette(
+                                findPalette(match.params.id)
+                            )}
+                        />
+                    )}
+                />
+            </Switch>
         </div>
     );
 };
